ae896e5ef3472595568c2a5c48c4345ce2bfaa20
FetheredSerpent <144615238+FetheredSerpent@users.noreply.github.com>
2024-03-24 15:09:35 +0530

    Update linux build script

--- scripts/build_lnx.sh
+++ scripts/build_lnx.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
+export FULL_SOURCE_DIRECTORY=/work/qt-everywhere-src-6.5.2
+
 echo -e "nameserver 1.1.1.1\nnameserver 8.8.8.8\nnameserver 8.8.4.4\n" > /etc/resolv.conf
 apk update
 apk add alpine-sdk cmake perl ninja-build ninja-is-really-ninja openssl-dev ffmpeg-dev clang14-libclang
 
-cd /work
+cd $FULL_SOURCE_DIRECTORY
 cd qtbase
 mkdir out && cd out
 ../configure --help
